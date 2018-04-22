c7f6d29d0ad6a753fa544ccc87e0555109c9f168
azerothYang <azerothyangg@gmail.com>
2018-04-22 17:20:15 +0800

    play

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
-void fahrToCelsius(void);
-int main() {
-    int c;
-    c = getchar();
-    while (c != EOF){
-        putchar(c);
-        c = getchar();
+#define IN 0 /*表示这个字符是字母*/
+#define OUT 1 /*表示这个字符不是字母*/
+int main(void){
+    int nc,nw,nl,c,state;
+    nc = nw = nl = 0;
+    state = OUT;
+    while ((c = getchar()) != ']'){
+        nc++; //字符数自增
+        //如果遇到换行, 行数加一
+        if (c == '\n'){
+            nl++;
+        }
+        //当前一个字符不是字母, 当前字符是字母的时候, 单词数自增1
+        if( (c >= 65 && c<=90) || (c>=97 && c<=122)){
+            putchar(c);
+            if (state == OUT){
+                state = IN;
+                nw++;
+            }
+        }
+        else {
+            printf("\n");
+            state = OUT;
+        }
     }
-//    fahrToCelsius();
-    return 0;
+    printf("nc: %d, nw: %d, nl: %d\n", nc, nw, nl);
 }
 
-// fahr to Celsius
-void fahrToCelsius(void){
-    float fahr=0, Celsius;
-    int step = 20;
-    while (fahr <= 300){
-        Celsius = (5*(fahr-32))/9;
-        printf("%3.0f %6.1f\n", fahr, Celsius);
-        fahr += step;
-    }
-}
